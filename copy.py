a1dbaa34fa5e6f8b86ca9393ef311f69bb530f73
manikanta76264 <40059381+manikanta76264@users.noreply.github.com>
2019-03-15 16:31:43 +0530

    Create copy.py

---
+++ copy.py
@@ -0,0 +1,3 @@
+import shutil
+shutil.copy('C:\Program Files (x86)\Jenkins\workspace\maven_git_pipeline\target' 'C:\Program Files\Apache Software Foundation\Tomcat 8.0\webapps\wedapp')
+print("done manikanta")
