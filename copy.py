547bc1c612c720d72f122c5571ee27d085312ecb
manikanta76264 <40059381+manikanta76264@users.noreply.github.com>
2019-03-15 16:42:54 +0530

    Update copy.py

--- copy.py
+++ copy.py
@@ -1,3 +1,3 @@
-import shutil
-shutil.copy('C:\\Program Files (x86)\\Jenkins\workspace\\maven_git_pipeline\\target\\*.war','C:\\Program Files\\Apache Software Foundation\\Tomcat 8.0\\webapps')
+import os
+os.copy('C:\\Program Files (x86)\\Jenkins\workspace\\maven_git_pipeline\\target\\*.war','C:\\Program Files\\Apache Software Foundation\\Tomcat 8.0\\webapps')
 print("done manikanta")
