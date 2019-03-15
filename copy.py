0270396302eb3033df108afcb9e610aa4a68b326
manikanta76264 <40059381+manikanta76264@users.noreply.github.com>
2019-03-15 16:59:10 +0530

    Update copy.py

--- copy.py
+++ copy.py
@@ -1,3 +1,6 @@
 import shutil
-shutil.copy('C:\\Program Files (x86)\\Jenkins\workspace\\maven_git_pipeline\\target\\sivamani.war','C:\\Program Files\\Apache Software Foundation\\Tomcat 8.0\\webapps')
-print("done manikanta")
+import os
+fil=os.listdir('C:\\Program Files (x86)\\Jenkins\workspace\\maven_git_pipeline\\target')
+if fil endswith('*.war'):
+    shutil.copy(fil,'C:\\Program Files\\Apache Software Foundation\\Tomcat 8.0\\webapps')
+    print("done manikanta")
