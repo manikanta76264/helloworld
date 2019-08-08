0e99d6bb200d91cfa75383dc8363ac4570b80df5
manikanta76264 <manikanta76264>
2019-08-08 11:47:26 +0530

    added paramiko remote connection file

--- copy.py
+++
@@ -1,7 +0,0 @@
-import shutil
-import os
-files=os.listdir('C:\\Program Files (x86)\\Jenkins\workspace\\maven_git_pipeline\\target')
-for source in files:
-    if source=='*.war':
-        shutil.copy(source,'C:\\Program Files\\Apache Software Foundation\\Tomcat 8.0\\webapps')
-        print("done manikanta")
