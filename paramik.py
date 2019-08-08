d5a272514cc0212b43d1415fdd3c508c36692a8c
manikanta76264 <manikanta76264>
2019-08-08 12:24:55 +0530

    renaming func in

--- paramik.py
+++ paramik.py
@@ -8,6 +8,7 @@ version=str(sys.argv[4])
 package_path=str(sys.argv[5])
 ssh=paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+print(package_paths)
 
 def ftp(path):
 	sftp=ssh.open_sftp()
