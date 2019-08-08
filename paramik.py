b83e9b73b14d51e996fd6fc176fa3ae3dd115a80
manikanta76264 <40059381+manikanta76264@users.noreply.github.com>
2019-08-08 19:05:12 +0530

    Update paramik.py

--- paramik.py
+++ paramik.py
@@ -8,17 +8,17 @@ version=str(sys.argv[4])
 package_path=str(sys.argv[5])
 pt="{}\{}".format(package_path,"target\{}".format(version)
                   
-con=paramiko.SSHClient()
+#con=paramiko.SSHClient()
                   
-con.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+paramiko.SSHClient().set_missing_host_key_policy(paramiko.AutoAddPolicy())
 def ftp(path):
-    sftp=con.open_sftp()
+    sftp=paramiko.SSHClient().open_sftp()
     sftp.put(pt,"/home/mani/tomcat/packages/")
     sftp.close()
 
 for host in hosts.split(","):
     print("conecting to {}".format(host))
-    con.connect(hostname=host,username="mani",password=pwd)
+    paramiko.SSHClient().connect(hostname=host,username="mani",password=pwd)
     ftp(package_path)
-    stdin,stdout,stderr=con.exec_command("sh /home/mani/tomcat/scripts/deploy.sh {} {}".format(instance,version),get_pty=True)
+    stdin,stdout,stderr=paramiko.SSHClient().exec_command("sh /home/mani/tomcat/scripts/deploy.sh {} {}".format(instance,version),get_pty=True)
     print(''.join(stdout.readlines()))
