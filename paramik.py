943e574e7989bf2bfe1ceac0e986b7162e063e24
manikanta76264 <40059381+manikanta76264@users.noreply.github.com>
2019-08-08 19:02:54 +0530

    Update paramik.py

--- paramik.py
+++ paramik.py
@@ -8,17 +8,17 @@ version=str(sys.argv[4])
 package_path=str(sys.argv[5])
 pt="{}\{}".format(package_path,"target\{}".format(version)
                   
-ssh = paramiko.SSHClient()
+con=paramiko.SSHClient()
                   
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+con.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 def ftp(path):
-    sftp=ssh.open_sftp()
+    sftp=con.open_sftp()
     sftp.put(pt,"/home/mani/tomcat/packages/")
     sftp.close()
 
 for host in hosts.split(","):
     print("conecting to {}".format(host))
-    ssh.connect(hostname=host,username="mani",password=pwd)
+    con.connect(hostname=host,username="mani",password=pwd)
     ftp(package_path)
-    stdin,stdout,stderr=ssh.exec_command("sh /home/mani/tomcat/scripts/deploy.sh {} {}".format(instance,version),get_pty=True)
+    stdin,stdout,stderr=con.exec_command("sh /home/mani/tomcat/scripts/deploy.sh {} {}".format(instance,version),get_pty=True)
     print(''.join(stdout.readlines()))
